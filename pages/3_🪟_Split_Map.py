4e78442b6d8e9fa84366a0569f45adf475e72ab4
Naufal Prawironegoro <56763735+NormanMul@users.noreply.github.com>
2023-10-10 02:43:01 +0700

    Update 3_🪟_Split_Map.py

--- pages/3_🪟_Split_Map.py
+++ pages/3_🪟_Split_Map.py
@@ -17,7 +17,7 @@ hide_st_style = """
             """
 st.markdown(hide_st_style, unsafe_allow_html = True)
 
-st.sidebar.title("Contact")
+st.sidebar.title("Xenox")
 st.sidebar.info(
     """
     
@@ -30,7 +30,7 @@ with st.expander("See source code"):
     with st.echo():
         m = leafmap.Map()
         m.split_map(
-            left_layer='ESA WorldCover 2020 S2 FCC', right_layer='ESA WorldCover 2020'
+            left_layer='ESA WorldCover 2022 S2 FCC', right_layer='ESA WorldCover 2021 V2'
         )
         m.add_legend(title='ESA Land Cover', builtin_legend='ESA_WorldCover')
 
