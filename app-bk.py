18d0adf90d91dbeaa4289ad93b3c680cc2f63122
Naufal Prawironegoro <56763735+NormanMul@users.noreply.github.com>
2023-10-10 03:10:13 +0700

    Update app-bk.py

--- app-bk.py
+++ app-bk.py
@@ -10,7 +10,7 @@ from apps import (
     gee_datasets,
     heatmap,
     home,
-    # hurricane,
+    hurricane,
     plotly_maps,
     raster,
     timelapse,
@@ -28,7 +28,9 @@ apps = MultiApp()
 
 apps.add_app("Home", home.app)
 apps.add_app("Create Timelapse", timelapse.app)
-# apps.add_app("Hurricane Mapping", hurricane.app)
+apps.add_app("Create Timelapse", timelapse.app)
+apps.add_app("world risk map", world_risk_map.app)
+apps.add_app("Hurricane Mapping", hurricane.app)
 apps.add_app("U.S. Census Data", census.app)
 apps.add_app("Visualize Raster Data", raster.app)
 apps.add_app("Visualize Vector Data", vector.app)
